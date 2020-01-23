d8648ab115a7d6835f3384fed466f75c5bb5fe67
Terique915 <54835492+Terique915@users.noreply.github.com>
2020-01-23 12:16:43 +0100

    Opdracht1

---
+++ Mijnjavascript2.js
@@ -0,0 +1,4 @@
+document.getElementById("Paginatitel").innerText =  "Mijn Pagina Titel"
+document.getElementById("subkop").innerText =  "Mijn Subkop"
+document.getElementById("img1").src = "img/Becky G.jpg"
+document.getElementById("img2").src = "img/logic1.jpg"
