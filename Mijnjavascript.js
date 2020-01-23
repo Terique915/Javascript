3e78ff40579da81c0af7fe23ad6de1cbf5f3a0f9
Terique915 <54835492+Terique915@users.noreply.github.com>
2020-01-23 09:52:21 +0100

    Opdracht1

--- Mijnjavascript.js
+++ Mijnjavascript.js
@@ -5,7 +5,11 @@ if(a <10 ){
     console.log("a is groter dan 9");
 }
 for(var i=0; i< 10; i++ ){
-    console.log("De waarde van a "+i);
+    console.log("De waarde van a = "+i);
+}var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    AantalWielen: 4,
+    kleur: "rood",
 }
-
-
+console.log(mijnauto.kleur);
