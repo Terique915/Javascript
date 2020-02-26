dbbbd9b7afbcef812f3972cda1801eb2b604599b
Terique915 <54835492+Terique915@users.noreply.github.com>
2020-02-26 12:16:38 +0100

    Opdracht1

---
+++ H02/Mijnjavascript2.2.js
@@ -0,0 +1,10 @@
+document.getElementById("b11").style.backgroundColor = "aqua";
+document.getElementById("b12").style.backgroundColor = "Green";
+document.getElementById("b13").style.backgroundColor = "yellow";
+document.getElementById("b14").style.backgroundColor = "red";
+
+
+document.getElementById("b12").style.marginTop = "20px";
+document.getElementById("b13").style.marginTop = "50px";
+
+
